1064f26cb61745511df896577b793804bdeb3f6f
HustAhq <134574955@qq.com>
2017-05-25 20:43:01 +0800

    two

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-/*
-var App = React.createClass({
+import createReactClass from 'create-react-class';
+
+
+/*var App = createReactClass({
 	getDefaultProps : function () {
 		console.log('getDefaultProps');
 		return {
@@ -9,66 +11,83 @@ var App = React.createClass({
 		}
 	},
 	getInitialState : function () {
-		console.log('getInitialState');
+        console.log('getInitialState');
 		return {
-			value : 'this is state'
+			value: 'input value'
 		}
 	},
-	componentWillMount : function () {
+	componentWillMount: function () {
 		console.log('componentWillMount');
 	},
 	componentDidMount : function () {
-		var _self = this;
 		console.log('componentDidMount');
-		window.setTimeout(function () {
-           _self.setState({value:'ffff', jiajia:'w982'})
-		}, 2000);
+		window.setTimeout( ()=> {
+           this.setState({
+			   value : 'nihao',
+			   age : 19
+		   })
+		}, 2000)
 	},
 	shouldComponentUpdate : function (nextProps, nextState) {
-		console.log('shouldComponentUpdate', nextProps, nextState);
+        console.log('shouldComponentUpdate');
+		console.log(nextProps, nextState);
+		if(nextState !== 'ddd'){
+			return false;
+		}
 		return true;
 	},
-	ComponentWillUpdate:function () {
-		console.log('ComponentWillUpdate');
+	componentWillUpdate: function () {
+       console.log('componentWillUpdate');
 	},
-	componentDidUpdate : function () {
-		console.log('componentDidUpdate')
+	componentDidUpdate:function () {
+		console.log('componentDidUpdate');
 	},
-	render: function () {
-		var data = 'duyi';
-		// console.log(this)
-		console.log(this.props.name);
-		return <div>
-		           <h1>{data}</h1>
-				   <ul>
-					   {
-						   this.props.data.map(function (ele, index){
-							   return <li key={index}>{ele}</li>
-						   })
-					   }
-				   </ul>
-			   </div>;
+    render : function () {
+		console.log('render')
+		var Cpname = 'duyi';
+		return (
+			<div>
+				<h1>{Cpname}</h1>
+				<ul>
+					{
+						this.props.data.map(function (ele, index) {
+                           return <li key={index}>{ele}</li>
+						})
+					}
+				</ul>
+			</div>
+		)
 	}
 });
 
-var dataList = ['js', 'css', 'html'];
+var dataList = ['js', 'html', 'react'];
+
 ReactDom.render(
-	<App data = {dataList}/>, 
+	<App data={dataList}/>,
 	document.getElementById('root')
-);*/
+)*/
 
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}</h2>
-    </div>
-  );
-  ReactDom.render(
-    element,
-    document.getElementById('root')
-  );
-}
+var Search = createReactClass({
+	getInitialState : function () {
+		return {
+			value : 'please input'
+		}
+	},
+	onhandleChange : function () {
+       this.setState({value:this.refs.inp.value})
+	},
+	render : function () {
+		console.log(this.state.value);
+		return (
+			<div>
+				<input type="text" ref='inp' value={this.state.value} onChange={this.onhandleChange}></input>
+				<span>{this.state.value}</span>
+			</div>
+		)
+	}
+})
 
-setInterval(tick, 1000);
-// tick();
+ReactDom.render(
+	<Search/>,
+	document.getElementById('root')
+)
